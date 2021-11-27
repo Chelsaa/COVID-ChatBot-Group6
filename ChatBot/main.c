63ae532569e6cbb4816c8e2cef0f42b093919040
Raveesh Yadav <raveeshyadav8@outlook.com>
2021-11-27 20:19:18 +0530

    Main script

--- ChatBot/main.c
+++ ChatBot/main.c
@@ -3,3 +3,51 @@
  * GROUP: 16
  * COPYRIGHT © GROUP 16
 */
+
+#include <stdio.h>
+
+int main(){
+    int userInput;
+
+    // Setting up the home page of the bot
+    // The home page consists of main menu and title.
+
+    printf("\n=====COVID-19 Chat-Bot=====\n");
+    printf("\nMAIN MENU\n\n1. Hospital related queries\n2. Home care related queries\n3. Food and medicine");
+    printf("\n\nEnter your choice to proceed: ");
+    scanf("%d", &userInput);
+
+    // The loop will only proceed if user input is either 1, 2 or 3.
+    // If input is not any of the mentioned number, the loop will
+    // end the user will automatically exit the bot.
+
+    while (userInput == 1 || userInput == 2 || userInput == 3){
+        
+        // Running the hospital questions script if user input is 1 
+        
+        if (userInput == 1){
+            printf("Hospital script");
+            break;
+        }
+
+        // Running the home care question script if the user input is 2
+
+        else if (userInput == 2){
+            printf("Home care script");
+            break;
+        }
+
+        // Running the food and medicine question script if user input is 3
+
+        else if (userInput == 3){
+            printf("Food and medicine script");
+            break;
+        }
+
+        else{
+            break;
+        }
+    }
+
+    return 0;
+}
